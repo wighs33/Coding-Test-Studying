c42ba24e21ba83bc9da6be84fde3c5260ee5d57b
wighs33 <genji@kakao.com>
2023-01-03 03:23:05 +0900

    다리를 지나는 트럭 알고리즘 수정

--- 010. 다리를 지나는 트럭/소스.cpp
+++ 010. 다리를 지나는 트럭/소스.cpp
@@ -1,5 +1,6 @@
 //스택&큐
 //항상 수도코드부터 작성하기
+//반복문에서 결과값이 계산 가능한 시점에 리턴하기
 
 #include <string>
 #include <vector>
@@ -10,37 +11,37 @@ using namespace std;
 
 int solution(int bridge_length, int weight, vector<int> truck_weights) {
 	int weight_in_bridge = 0;
-	queue<int> bridge;
-	int second = 0;
+	queue<pair<int, int>> bridge;
+	int seconds = 0;
+
 	for (size_t i = 0; ;)
 	{
-		if (i < truck_weights.size())
+		if (weight >= weight_in_bridge + truck_weights[i])
 		{
+			bridge.push(make_pair(truck_weights[i], seconds));
 			weight_in_bridge += truck_weights[i];
-			if (weight < weight_in_bridge)
-			{
-				weight_in_bridge -= truck_weights[i];
-				bridge.push(0);
-			}
-			else
-			{
-				bridge.push(truck_weights[i]);
-				++i;
-			}
+			++i;
 		}
 
-		if (bridge.size() == bridge_length)
+		if (i >= truck_weights.size())
 		{
-			weight_in_bridge -= bridge.front();
-			bridge.pop();
-			++second;
+			return bridge_length + 1 + seconds;
 		}
-		if (i == truck_weights.size())
-			return second + bridge.size() + bridge_length;
+		else
+		{
+			++seconds;
+			if (bridge.front().second + bridge_length == seconds)
+			{
+				weight_in_bridge -= bridge.front().first;
+				bridge.pop();
+			}
+		}
 	}
 }
 
 int main()
 {
+	cout << solution(2, 10, { 7,4,5,6 }) << endl;
+	cout << solution(100, 100, { 10,10,10,10,10,10,10,10,10,10 }) << endl;
 	cout << solution(100, 100, { 10,10,10,10,10,10,10,10,10,10 }) << endl;
 }
