b4057d67c83db4051d9aee2394834bc5fb0105f6
wighs33 <genji@kakao.com>
2023-01-03 18:33:38 +0900

    알고리즘 더 효율성 있게 수정

--- 013. 디스크 컨트롤러/소스.cpp
+++ 013. 디스크 컨트롤러/소스.cpp
@@ -11,33 +11,29 @@
 
 using namespace std;
 
-bool compare(vector<int> a, vector<int> b) {
-    return a[1] < b[1];
-}
-
 int solution(vector<vector<int>> jobs) {
-    int answer = 0;
-    int start = 0; // 현재까지 작업이 진행된 시간
-    int time = 0; // 각각의 작업이 진행되는데 걸린 시간들의 합
+    int total_time = 0;
+    int work_time = 0;
     int size = jobs.size();
 
-    sort(jobs.begin(), jobs.end(), compare); // 소요시간으로 우선 배열
+    sort(jobs.begin(), jobs.end(), [](vector<int> a, vector<int> b) {
+        return a[1] < b[1];
+        }); // 소요시간으로 우선 배열
 
-    while (!jobs.empty()) {
+    while (true) {
         for (int i = 0; i < jobs.size(); i++) {
-            if (jobs[i][0] <= start) {
-                start += jobs[i][1];
-                time += start - jobs[i][0];
+            if (jobs[i][0] <= total_time) {
+                total_time += jobs[i][1];
+                work_time += total_time - jobs[i][0];
+                if (jobs.size() == 1)
+                    return work_time / size;
                 jobs.erase(jobs.begin() + i);
                 break;
             }
 
-            if (i == jobs.size() - 1) start++;
+            if (i == jobs.size() - 1) total_time++;
         }
     }
-
-    answer = time / size;
-    return answer;
 }
 
 int main()
