8e1434d940c3f377652f8cd8645c812277903d84
wighs33 <genji@kakao.com>
2023-01-04 21:51:02 +0900

    h인덱스 문제 해결

---
+++ 017. H-Index/소스.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+#include <iostream>
+
+using namespace std;
+
+int solution(vector<int> citations) {
+    sort(citations.begin(), citations.end(), greater<int>());
+
+    for (int i = 0; i < citations.size(); ++i) {
+        if (citations[i] < i + 1) {
+            return i;
+        }
+    }
+
+    return citations.size();
+}
+
+int main()
+{
+    cout << solution({ 3, 0, 6, 1, 5 }) << endl;
+}
