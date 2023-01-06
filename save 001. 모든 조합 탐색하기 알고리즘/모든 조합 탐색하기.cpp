f26030d678bf054192d70cada4a46cd138d0da7c
wighs33 <genji@kakao.com>
2023-01-07 01:45:43 +0900

    dfs알고리즘 지우고 실용성 있는 알고리즘 저장하기

---
+++ save 001. 모든 조합 탐색하기 알고리즘/모든 조합 탐색하기.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+void mix_func(string mix, int pos, string numbers)
+{
+	//사이즈까지 모든 조합
+	//cout << "mix: " << mix << endl;
+
+	//사이즈가 채워질 때 조합
+	if (pos == mix.size())
+	{
+		//cout << "mix: " << mix << endl;
+		return;
+	}
+
+	for (size_t i = 0; i < numbers.size(); i++)
+	{
+		string a = numbers;
+		string m = mix;
+		int p = pos;
+		m[p] = a[i];
+		a.erase(a.begin() + i);
+
+		mix_func(m, ++p, a);
+	}
+}
+
+int main()
+{
+	string numbers = "01234";
+
+	string mix;
+	mix.resize(numbers.size());
+	mix_func(mix, 0, numbers);
+}
